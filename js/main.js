589c95cceacf92fc1cc4e38cda070c00797334a1
Axrorbek-023 <axrorbek2231@gmail.com>
2024-02-15 22:04:04 +0500

    carusel

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var elWrapper = document.querySelector('.wrapper')
+var elCorusel = document.querySelector('.corusel__list')
+var elCoruselItems = document.querySelectorAll('.corusel__item')
+var sz = elWrapper.offsetWidth
+var len = elCoruselItems.length;
+var count = 0
+elCorusel.style.width = `${100*len}%`
+elCorusel.style.hight = `100%`
+for(var i = 0; i< elCoruselItems.length; i++){
+    elCoruselItems[i].style.width = `${100 / len}%`
+    elCoruselItems[i].style.hight = '100%'
+}
+function fnPrevius(){
+    count -= 1
+    if(count > 1){
+        elCorusel.style.transform = `translateX(-${sz * count}px)`
+    }else{
+        count = 1
+    }
+}
+
+function fnNext(){
+    count += 1
+    if(count < len){
+        elCorusel.style.transform = `translateX(-${sz * count}px)`
+    }else{
+        count = len - 1
+    }
+}
